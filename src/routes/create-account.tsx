db4af3aa44612b4219ede45a6982e094f71c5e78
Juferis <66511872+Juferis@users.noreply.github.com>
2025-02-11 23:18:19 +0900

    Create account form and UI css

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,10 +1,42 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 
-const Wrapper = styled.div``;
-const Title = styled.h1``;
-const Form = styled.form``;
-const Input = styled.input``;
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 420px;
+  padding: 50px 0px;
+`;
+const Title = styled.h1`
+  font-size: 42px;
+`;
+const Form = styled.form`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`;
+const Input = styled.input`
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: none;
+  width: 100%;
+  font-size: 16px;
+  &[type='submit'] {
+    cursor: pointer;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+
+const Error = styled.span`
+  font-weight: 600;
+  color: red;
+`;
 
 export default function CreatAccount() {
   const [isLoading, setIsLoading] = useState(false);
@@ -71,6 +103,7 @@ export default function CreatAccount() {
           <Input type="submit" value="Create Account" />
         )}
       </Form>
+      {error !== '' ? <Error>{error}</Error> : null}
     </Wrapper>
   );
 }
