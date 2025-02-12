6cdf19fed085d4e3e88e650afbab85106bd3ead4
Juferis <66511872+Juferis@users.noreply.github.com>
2025-02-12 23:52:37 +0900

    create constant.ts

---
+++ src/constant.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_ERROR_MESSAGE: Record<string, string> = {
+    "auth/email-already-in-use": "이미 사용 중인 이메일입니다.",
+    "auth/invalid-email": "유효하지 않은 이메일 형식입니다.",
+    "auth/user-not-found": "가입된 사용자가 없습니다.",
+    "auth/wrong-password": "비밀번호가 일치하지 않습니다."
+};
+
+export const NOT_FOUND_ERROR = "예기치 않은 오류가 발생했습니다.";
