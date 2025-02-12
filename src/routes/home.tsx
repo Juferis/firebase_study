e25b10d92d7ddc1285b637f3dd41e26f0f15c7ce
Juferis <66511872+Juferis@users.noreply.github.com>
2025-02-12 23:39:16 +0900

    Write logout event

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,7 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../firebase';
+import { ROUTES } from '../routes';
+
 export default function Home() {
+  const navigate = useNavigate();
+
+  const logOut = async () => {
+    await auth.signOut();
+    navigate(ROUTES.LOGIN);
+  };
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <h1>
+      <button onClick={logOut}>Log Out</button>
+    </h1>
   );
 }
