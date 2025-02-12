114e9790c6f5f8dc69493fa667a07ca0699229dd
Juferis <66511872+Juferis@users.noreply.github.com>
2025-02-13 00:01:05 +0900

    add firebase error message

--- src/constant.ts
+++ src/constant.ts
@@ -3,6 +3,8 @@ export const FIREBASE_ERROR_MESSAGE: Record<string, string> = {
     "auth/invalid-email": "유효하지 않은 이메일 형식입니다.",
     "auth/user-not-found": "가입된 사용자가 없습니다.",
     "auth/wrong-password": "비밀번호가 일치하지 않습니다."
+      "auth/weak-password": "비밀번호가 너무 약합니다. 강력한 비밀번호를 사용하세요.",
+    "auth/missing-password": "비밀번호를 입력해주세요.",
 };
 
 export const NOT_FOUND_ERROR = "예기치 않은 오류가 발생했습니다.";
