eb67dce418f12db5379fc7fe49aa5a57390626b4
Juferis <66511872+Juferis@users.noreply.github.com>
2025-02-13 00:12:43 +0900

    Create Login page, mdify create-account page

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,49 +1,18 @@
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { useState } from 'react';
-import styled from 'styled-components';
 import { auth } from '../firebase';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { ROUTES } from '../routes';
 import { FirebaseError } from 'firebase/app';
 import { FIREBASE_ERROR_MESSAGE, NOT_FOUND_ERROR } from '../constant';
-
-const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 420px;
-  padding: 50px 0px;
-`;
-const Title = styled.h1`
-  font-size: 42px;
-`;
-const Form = styled.form`
-  margin-top: 50px;
-  margin-bottom: 10px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-`;
-const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
-  font-size: 16px;
-  &[type='submit'] {
-    cursor: pointer;
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`;
-
-const Error = styled.span`
-  font-weight: 600;
-  color: red;
-`;
+import {
+  Input,
+  Switcher,
+  Title,
+  Wrapper,
+  Error,
+  Form,
+} from '../components/auth-components';
 
 export default function CreatAccount() {
   const navigate = useNavigate();
@@ -126,6 +95,10 @@ export default function CreatAccount() {
         />
       </Form>
       {error !== '' ? <Error>{error}</Error> : null}
+      <Switcher>
+        이미 계정이 이으신가요?
+        <Link to={ROUTES.LOGIN}>로그인하기 &rarr;</Link>
+      </Switcher>
     </Wrapper>
   );
 }
