8c94170f746cef61e98445c8c11bf6357c20fc5e
Juferis <66511872+Juferis@users.noreply.github.com>
2025-02-14 11:41:40 +0900

    Create Post tweets method

---
+++ src/components/post-tweet-form.tsx
@@ -0,0 +1,130 @@
+import { addDoc, collection } from 'firebase/firestore';
+import { useState } from 'react';
+import { styled } from 'styled-components';
+import { auth, db } from '../firebase';
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+const TextArea = styled.textarea`
+  border: 2px solid white;
+  padding: 20px;
+  border-radius: 20px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  height: 200px;
+  resize: none;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  &::placeholder {
+    font-size: 16px;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+const AttachFileButton = styled.label`
+  cursor: pointer;
+  padding: 10px 0px;
+  color: #1d9bf0;
+  text-align: center;
+  border-radius: 20px;
+  border: 1px solid #1d9bf0;
+  font-size: 14px;
+  font-weight: 600;
+`;
+const AttachFileInput = styled.input`
+  display: none;
+`;
+const SubmitButton = styled.input`
+  cursor: pointer;
+  background-color: #1d9bf0;
+  color: white;
+  border: none;
+  padding: 10px 0px;
+  border-radius: 20px;
+  font-size: 16px;
+  &:hover,
+  &:active {
+    opacity: 0.9;
+  }
+`;
+
+export default function PostTweetForm() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [tweet, setTweet] = useState('');
+  const [file, setFile] = useState<File | null>(null);
+
+  const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTweet(event.target.value);
+  };
+  const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = event.target;
+    // TODO: 파일 최대 4개 업로드로 변경
+    if (files && files.length === 1) {
+      setFile(files[0]);
+    }
+  };
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const user = auth.currentUser;
+    if (!user || isLoading || tweet === '' || tweet.length > 180) return;
+
+    try {
+      setIsLoading(true);
+      await addDoc(collection(db, 'posts'), {
+        tweet,
+        createdAt: Date.now(),
+        username: user?.displayName || 'Anonymous',
+        userId: user?.uid,
+      });
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <TextArea
+        onChange={onChange}
+        placeholder="무슨일이 일어나고 있나요?"
+        value={tweet}
+        rows={5}
+        maxLength={180}
+      />
+      <AttachFileButton htmlFor="file">
+        {/* <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="size-6"
+        >
+          <path
+            fillRule="evenodd"
+            d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
+            clipRule="evenodd"
+          />
+        </svg> */}
+        {file ? '사진 업로드 완료' : '사진 업로드'}
+      </AttachFileButton>
+      <AttachFileInput
+        onChange={onFileChange}
+        id="file"
+        type="file"
+        accept="image/*"
+      />
+      <SubmitButton
+        type="submit"
+        value={isLoading ? '업로드중...' : '작성하기'}
+      />
+    </Form>
+  );
+}
